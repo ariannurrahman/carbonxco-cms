2646864a9382fc1eb8436f40141a6603d59d3090
arian <arian@MacBook-Pro.local>
2023-09-01 16:40:49 +0700

    Disable context

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,15 @@
 import { Suspense } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
-import { AuthContext } from 'context/AuthContext';
-import { useAuth } from 'hooks/useAuth';
 import AppRoutes from 'routes';
 
 import './App.scss';
 
 function App() {
-  const { user, setUser } = useAuth();
-
   return (
     <BrowserRouter>
       <Suspense fallback={<p>Loading...</p>}>
-        <AuthContext.Provider value={{ user, setUser }}>
-          <AppRoutes />
-        </AuthContext.Provider>
+        <AppRoutes />
       </Suspense>
     </BrowserRouter>
   );
